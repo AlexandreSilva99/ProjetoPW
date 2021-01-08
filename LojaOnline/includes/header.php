4dc9306ec34fe9694967db399b377bacf40b8724
AlexandreSilva99 <63048987+AlexandreSilva99@users.noreply.github.com>
2021-01-08 11:10:37 +0000

    Add files via upload

---
+++ LojaOnline/includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- Header -->
+    <div id="shadow">
+    <div id="header">
+        <div id="headerleft">
+            <a href="index.php">Pro Detail</a>
+        </div>
+
+        <div id="search">
+        </div>
+
+        <div id="headerrigth">
+            <div class="topRigth"></div>
+            <div class="topRigth"></div>
+            <div class="topRigth"></div>
+        </div>
+    <!-- Fim do Header -->
+
+</body>
